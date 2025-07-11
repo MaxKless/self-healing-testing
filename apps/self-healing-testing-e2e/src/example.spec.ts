396de0b2ff8b98ce82c71b97856dc0907860751b
Max Kless <maxk@nrwl.io>
2025-07-11 12:25:32 +0200

    another change

--- apps/self-healing-testing-e2e/src/example.spec.ts
+++ apps/self-healing-testing-e2e/src/example.spec.ts
@@ -3,7 +3,8 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  console.log('heyaaa123')
+  console.log('heyaaa1234')
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).not.toContain('Welcome');
 });
+
