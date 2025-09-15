82576762375ae71370e7ea7b842d4b613671f513
Max Kless <maxk@nrwl.io>
2025-09-15 14:55:50 +0200

    fix the thing

--- apps/self-healing-testing/src/main.tsx
+++ apps/self-healing-testing/src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-
+if (true) {
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
